ccabc111844827e84768d82374738bb06f7402ea
Julian Quispe <julianmql@hotmail.com>
2023-10-12 11:42:32 -0500

    feat: charts library added

--- app/(general)/prueba/page.tsx
+++ app/(general)/prueba/page.tsx
@@ -1,4 +1,10 @@
+import { DataCard } from "../../../components/DataCard/DataCard";
+import { DonutChart } from "../../../components/DonutChart/DonutChart";
+
 export default function PruebaPage() {
-    return <div>Esto es una prueba de contenido</div>;
+    return <div>
+        Esto es una prueba de contenido
+        <DonutChart></DonutChart>
+    </div>;
   }
   
