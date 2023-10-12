d9212ea734dfd16f527099d79e77988525108f4c
Julian Quispe <julianmql@hotmail.com>
2023-10-12 12:39:22 -0500

    feat: login page added

---
+++ app/login/page.tsx
@@ -0,0 +1,6 @@
+import { AuthenticationImage } from "../../components/AuthenticationImage/AuthenticationImage";
+
+export default function LoginPage() {
+    return <AuthenticationImage></AuthenticationImage>;
+  }
+  
