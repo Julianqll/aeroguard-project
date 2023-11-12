9119b4deceba3a143be61b8f76b2e6e578ab8f2b
Paris Lorenzo <84421273+ParisLorenzo@users.noreply.github.com>
2023-11-11 23:38:41 -0500

    feat: select, insert & delete rol queries added

--- queries/rolQuery.ts
+++ queries/rolQuery.ts
@@ -1,10 +1,35 @@
 import { gql } from '@apollo/client';
 
+//gql - convertir query a string 
 export const GET_ROLS = gql`
-  query GetRols {
-    rol {
-        nombre
-        rolId
-      }
+query RolQuery 
+{
+  rol {
+    estado
+    idRol
+    nombre
   }
+}
+`;
+
+
+export const DELETE_ROLS = gql`
+mutation DeleteRol($_eq: Int) 
+{
+  delete_rol(where: {idRol: {_eq: $_eq}}) {
+    returning {
+      idRol
+      nombre
+    }
+  }
+}
+`;
+
+export const INSERT_ROLS = gql`
+mutation INSERT_ROL($object: rol_insert_input = {}) {
+  insert_rol_one(object: $object) {
+    idRol
+    nombre
+  }
+}
 `;
